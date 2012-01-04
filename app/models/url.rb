2f91037caf355f23bed619818c220f382843afdc
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-01-04 00:04:10 -0200

    Added the Mongoid Timestamps

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,11 +1,10 @@
 class Url
     include Mongoid::Document
+    include Mongoid::Timestamps
     field :permalink, :type => String
     field :token, :type => String
     field :ip_address, :type => String
-    field :created_at, :type => Time
-    field :updated_at, :type => Time
-
+    
     validates :permalink, :token, :presence => true
     validate :permalink do
         if permalink !~ %r{^(http|https|git|svn)://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$}
